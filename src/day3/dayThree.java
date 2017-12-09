1ad78be88f39d191aa0a1b923e58271b54cd6dd1
Bobby <sgmaniac1255@yahoo.com>
2017-12-09 14:19:18 -0600

    work on day three

---
+++ src/day3/dayThree.java
@@ -0,0 +1,33 @@
+package day3;
+
+public class dayThree {
+
+	public static int input= 289326;
+	
+	
+	public static void main(String[] args) {
+		
+		buildMemory();
+		
+		System.out.println(partOne());
+		System.out.println(partTwo());
+
+	}
+	
+	private static void buildMemory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static String partOne(){
+		
+		
+		return "Day 3 part 1: ";
+	}
+	
+	public static String partTwo(){
+		
+		return "Day 3 part 2: ";
+	}
+
+}
