09cfa2976a31997d2d61815817da006ba369ac2d
Ranzer <sgmaniac1255@yahoo.com>
2018-02-27 08:16:44 -0600

    b

---
+++ src/y2015/day07/AssemblyRequired.java
@@ -0,0 +1,10 @@
+package y2015.day07;
+
+public class AssemblyRequired {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/y2016/day08/TwoFactor.java
@@ -0,0 +1,15 @@
+package y2016.day08;
+
+import java.io.File;
+
+public class TwoFactor {
+	
+	static File input = new File("C:/Users/jrdillingham.MABANKISD/Programing/workspace/adventofcode/src/y2016/day08/input.txt");
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
